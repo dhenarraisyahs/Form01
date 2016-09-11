a0a4bb8462fa2255c152093363297d0cb7ffb7ab
Dhenarra Erine Aisyahsari <dhenarra18@gmail.com>
2016-09-11 09:28:12 +0700

    Second Commit +Memfungsikan button, nama, dan tahun

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1010/form01/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1010/form01/MainActivity.java
@@ -2,11 +2,34 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1010.form01;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama, etTahun;
+    Button bOK;
+    TextView tvHasil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTahun = (EditText) findViewById(R.id.editTextTahun);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+                int tahun = Integer.parseInt(etTahun.getText().toString());
+                int usia = 2016 - tahun;
+                tvHasil.setText(nama + " berusia " + usia + " tahun");
+            }
+        });
     }
 }
